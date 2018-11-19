2568c3b311a124cffabce31d2ec9395c0c52617b
Johnny Quan <johnny@moxy.cloud>
2018-11-19 13:17:23 +1000

    update task controller

--- src/main/java/com/todoservice/gemfirerestapi/controllor/TasksController.java
+++ src/main/java/com/todoservice/gemfirerestapi/controllor/TasksController.java
@@ -1,10 +1,9 @@
 package com.todoservice.gemfirerestapi.controllor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.todoservice.gemfirerestapi.exception.ValidationErrorException;
 import com.todoservice.gemfirerestapi.model.BalanceTestResult;
 import com.todoservice.gemfirerestapi.services.ValidationService;
 
@@ -12,10 +11,8 @@ public class TasksController {
 	@Autowired
 	private ValidationService ValidationService;
 	
-	@RequestMapping("/tasks/validateBrackets")
+	@GetMapping("/tasks/validateBrackets")
 	public BalanceTestResult validateBrackets(@RequestParam(value = "input") String input) throws Exception{
-		if (input.length()>50 ||input.length()<1 ) 
-			throw new ValidationErrorException(input);
 		return ValidationService.getValidateBracketsModel(input);
 	}
 }
