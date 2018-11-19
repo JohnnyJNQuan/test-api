b913245fd3d5cc47027e127548abcfe387c424f3
Johnny Quan <johnny@moxy.cloud>
2018-11-19 14:13:58 +1000

    update todo patch

--- src/main/java/com/todoservice/gemfirerestapi/controllor/ToDoController.java
+++ src/main/java/com/todoservice/gemfirerestapi/controllor/ToDoController.java
@@ -15,6 +15,7 @@ import com.todoservice.gemfirerestapi.services.ValidationService;
 import com.todoservice.gemfirerestapi.model.BalanceTestResult;
 import com.todoservice.gemfirerestapi.model.ToDoItem;
 import com.todoservice.gemfirerestapi.model.ToDoItemAddRequest;
+import com.todoservice.gemfirerestapi.model.ToDoItemUpdateRequest;
 import com.todoservice.gemfirerestapi.repository.*;
 import com.todoservice.gemfirerestapi.exception.ItemNotFoundException;
 import com.todoservice.gemfirerestapi.exception.ValidationErrorException;;
@@ -31,22 +32,23 @@ public class ToDoController {
     }
 
 	@PostMapping("/todo")
-    public ResponseEntity<ToDoItem> postItem( @RequestBody ToDoItemAddRequest viewModel)throws Exception {
-		if (viewModel.getText().length()>50 ||viewModel.getText().length()<1 ) 
-			throw new ValidationErrorException(viewModel.getText());
+    public ResponseEntity<ToDoItem> postItem( @RequestBody ToDoItemAddRequest toDoItemAddRequest)throws Exception {
+		if (toDoItemAddRequest.getText().length()>50 ||toDoItemAddRequest.getText().length()<1 ) 
+			throw new ValidationErrorException(toDoItemAddRequest.getText());
 		ToDoItem toDoItem = new ToDoItem();
-		toDoItem.setText(viewModel.getText());
+		toDoItem.setText(toDoItemAddRequest.getText());
 		todoRepository.save(toDoItem);
     
     	 return new ResponseEntity<ToDoItem>(toDoItem, HttpStatus.OK);
     }
 	
 	@PatchMapping("/todo/{id}")
-    public ResponseEntity<ToDoItem> patchItem(@RequestBody ToDoItemAddRequest viewModel, @PathVariable("id") long id ) throws Exception {
-		if (viewModel.getText().length()>50 ||viewModel.getText().length()<1 ) 
-			throw new ValidationErrorException(viewModel.getText());
+    public ResponseEntity<ToDoItem> patchItem(@RequestBody ToDoItemUpdateRequest toDoItemUpdateRequest, @PathVariable("id") long id ) throws Exception {
+		if (toDoItemUpdateRequest.getText().length()>50 ||toDoItemUpdateRequest.getText().length()<1 ) 
+			throw new ValidationErrorException(toDoItemUpdateRequest.getText());
 		ToDoItem toDoItem = todoRepository.findById(id);
-		toDoItem.setText(viewModel.getText());
+		toDoItem.setText(toDoItemUpdateRequest.getText());
+		toDoItem.setIsCompleted(toDoItemUpdateRequest.isCompleted());
 		todoRepository.save(toDoItem);
     
     	 return new ResponseEntity<ToDoItem>(toDoItem, HttpStatus.OK);
