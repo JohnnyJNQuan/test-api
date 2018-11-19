a651cab2e54e684b72266bd77514778626c6db37
Johnny Quan <johnny@moxy.cloud>
2018-11-19 13:26:23 +1000

    update task controller

--- src/main/java/com/todoservice/gemfirerestapi/controllor/TasksController.java
+++ src/main/java/com/todoservice/gemfirerestapi/controllor/TasksController.java
@@ -3,10 +3,12 @@ package com.todoservice.gemfirerestapi.controllor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.todoservice.gemfirerestapi.model.BalanceTestResult;
 import com.todoservice.gemfirerestapi.services.ValidationService;
 
+@RestController
 public class TasksController {
 	@Autowired
 	private ValidationService ValidationService;
